93c1db0e983c3a5a9e41aa6a3f273d9022626a46
GabrielyAndrade <134338508+GabrielyAndrade@users.noreply.github.com>
2023-05-22 20:00:33 -0300

    Incluido atribuições na classe Aluno e criada classe AdmTelas

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,4 +1,13 @@
+import java.util.Calendar;
 
 public class Aluno {
-
+	//Atributos da classe Pessoa:
+	private String nome; // equivalente a "char nome[30]" utilizado em C
+	private String cpf;
+	private Calendar dataNascimento;
+	private String endereco;//(Rua,Número,Bairro,Cidade)
+    private String telefone;//(Telefone,E-mail)
+    private String codigo;
+    private float rendaMensal;
+    private String empregado;//(Empresa,cargo,etc.)
 }

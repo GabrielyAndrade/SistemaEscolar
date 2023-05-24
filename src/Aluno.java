44b25be4e7bbe6cf0d77905b4d99cf8c5a198bf7
GabrielyAndrade <134338508+GabrielyAndrade@users.noreply.github.com>
2023-05-24 20:35:26 -0300

    Atualizado codigo

--- src/Aluno.java
+++ src/Aluno.java
@@ -19,7 +19,16 @@ public class Aluno {
     private String telefone_responsavel;
     
     /**<br> Agora a classe terá os seguintes métodos:*/
-    
+    /**<br> Método Construtor*/
+    public Aluno(String nome,String CPF, String dataNascimento, String Endereco, String telefone_aluno,String responsavel,String telefone_responsavel) {
+    this.setNome(nome);
+    this.setCpf(CPF);
+    this.setdataNascimento(dataNascimento);
+    this.setEndereco(endereco);
+    this.setTelefone_aluno(telefone_aluno);
+    this.setResponsavel(responsavel);
+    this.setTelefone_responsavel(telefone_responsavel);
+    }
     /**<br> Método para alterar/definir o Nome do Aluno:*/ 
     public void setNome(String nome1){
         if(nome1.length()>0) 
@@ -33,7 +42,7 @@ public class Aluno {
         return this.nome;
     }
     
-    /**Método para alterar/definir o CPF do aluno:*/
+    /**<br>Método para alterar/definir o CPF do aluno:*/
     public void setCpf(String cpf1){
         if (cpf1.length()==11) {
             this.cpf=cpf1;
@@ -41,11 +50,11 @@ public class Aluno {
             System.out.println("Digite um número de CPF válido!"); // ADAPTAR PARA A TELA
         }
     }
-    /**Método para retornar o CPF do Aluno*/
+    /**<br>Método para retornar o CPF do Aluno*/
     public String getCpf(){
         return this.cpf;
     }
-    /**Método para alterar/definir a Data de Nascimento do Aluno*/
+    /**<br>Método para alterar/definir a Data de Nascimento do Aluno*/
     public String setdataNascimento(String dataNascimento1) {
 		int dia, mes, ano;
 	    Calendar data = Calendar.getInstance();
@@ -63,12 +72,12 @@ public class Aluno {
 	    }
 	    return "Erro desconhecido";
     }
-    /**Método que retorna a Data de Nascimento do Aluno*/
+    /**<br>Método que retorna a Data de Nascimento do Aluno*/
     public String getDataNascimento(){
     	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         return ""+ sdf.format(dataNascimento.getTime());
     }
-    /**Método que define/altera o Endereço do Aluno*/
+    /**<br>Método que define/altera o Endereço do Aluno*/
     public void setEndereco(String endereço1){
         if (endereço1.length()>0)
             this.endereco=endereço1;
@@ -76,15 +85,15 @@ public class Aluno {
             System.out.println("Digite um endereço válido!");
         }
     }
-    /**Método que retorna o Endereço do Aluno*/
+    /**<br>Método que retorna o Endereço do Aluno*/
     public String getEndereco(){
         return this.endereco;
     }
-    /**Método que retorna o Telefone do Aluno*/
+    /**<br>Método que retorna o Telefone do Aluno*/
     public String getTelefone(){
         return this.telefone_aluno;
     }
-    /**Método que define/altera o Telefone do Aluno*/
+    /**<br>Método que define/altera o Telefone do Aluno*/
 	public String setTelefone_aluno(String telefonealuno) {
         if (telefonealuno.length()==11) {
             this.telefone_aluno=telefonealuno;
@@ -93,7 +102,7 @@ public class Aluno {
             return("Digite um telefone no formato abaixo:\n 47912345678 (DDD 9 12345678)");
         }
 	}
-    /**Método que define/altera o Nome do Responsável do Aluno*/
+    /**<br>Método que define/altera o Nome do Responsável do Aluno*/
     public void setResponsavel(String responsavel1){
         if(responsavel1.length()>0) 
             this.responsavel = responsavel1;
@@ -101,15 +110,15 @@ public class Aluno {
             System.out.println("Digite um nome válido!"); // ADAPTAR PARA A TELA
         }
     }
-    /**Método que retorna o Nome do Responsável do Aluno*/
+    /**<br>Método que retorna o Nome do Responsável do Aluno*/
     public String getResponsavel(){
         return this.responsavel;
     }
-    /**Método que retorna o Telefone do Responsável do Aluno*/
+    /**<br>Método que retorna o Telefone do Responsável do Aluno*/
 	public String getTelefone_responsavel() {
 		return telefone_responsavel;
 	}
-    /**Método que define/altera o Telefone do Responsável do Aluno*/
+    /**<br>Método que define/altera o Telefone do Responsável do Aluno*/
 	public String setTelefone_responsavel(String telefoneresponsavel) {
         if (telefoneresponsavel.length()==11) {
             this.telefone_aluno=telefoneresponsavel;
